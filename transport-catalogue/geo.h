ccd9adb5e1acbfe57793b93da68e07b95d16e430
Даниил Мишарин <danila-master@inbox.ru>
2022-05-11 12:00:31 +0400

    v_1_1

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -11,14 +11,5 @@ namespace geo {
         bool operator!=(const Coordinates& other) const;
     };
 
-    inline double ComputeDistance(Coordinates from, Coordinates to) {
-        using namespace std;
-        if (from == to) {
-            return 0;
-        }
-        static const double dr = 3.1415926535 / 180.;
-        return acos(sin(from.lat * dr) * sin(to.lat * dr)
-            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-            * RADIUS_EARTH;
-    }
+    inline double ComputeDistance(Coordinates from, Coordinates to);
 }
